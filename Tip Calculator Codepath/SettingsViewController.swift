692c3bc4c26697becfc9cc0ec3ce8067c3960705
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-09-24 22:40:34 +0800

    Attempt dark mode but cannot make it...

--- Tip Calculator Codepath/SettingsViewController.swift
+++ Tip Calculator Codepath/SettingsViewController.swift
@@ -13,6 +13,7 @@ class SettingsViewController: UIViewController {
     var tipSelection = 0
     
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var colorControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,17 @@ class SettingsViewController: UIViewController {
     }
     
 
+    @IBAction func setColor(_ sender: Any) {
+        // dark mode not working :(
+//        if colorControl.selectedSegmentIndex == 1 {
+//                UILabel.appearance().textColor = UIColor.white
+//            self.view.backgroundColor = UIColor.black
+//
+//        } else {
+//            self.view.backgroundColor = UIColor.white
+//            UILabel.appearance().textColor = UIColor.black
+//        }
+    }
     /*
     // MARK: - Navigation
 
