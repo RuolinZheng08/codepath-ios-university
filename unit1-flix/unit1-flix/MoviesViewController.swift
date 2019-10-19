d2423c9a97eff93a78094cf9b0b9080c335ec5cb
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-10-18 19:09:02 -0500

    Start unit1 project

---
+++ unit1-flix/unit1-flix/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  unit1-flix
+//
+//  Created by Ruolin Zheng on 2019/10/18.
+//  Copyright © 2019 Ruolin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
