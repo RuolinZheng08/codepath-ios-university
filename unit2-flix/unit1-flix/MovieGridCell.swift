736a4e453f8d06e9ec7c70c7af70b9baec46b407
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-10-25 16:03:46 -0500

    Finish basic grid view

---
+++ unit2-flix/unit1-flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  unit2-flix
+//
+//  Created by Ruolin Zheng on 2019/10/25.
+//  Copyright © 2019 Ruolin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
