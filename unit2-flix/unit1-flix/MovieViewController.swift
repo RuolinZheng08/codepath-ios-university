747088c0adf34d6c27b948697d9d98d0c48377ea
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-10-25 15:21:50 -0500

    Finish detail screen

--- unit2-flix/unit1-flix/MovieViewController.swift
+++ unit2-flix/unit1-flix/MovieViewController.swift
@@ -6,16 +6,35 @@
 //  Copyright © 2019 Ruolin Zheng. All rights reserved.
 //
 
+import AlamofireImage
 import UIKit
 
 class MovieViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
     
     var movie: [String: Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: "https://image.tmdb.org/t/p/w185" + posterPath)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backdropView.af_setImage(withURL: backdropUrl!)
+        posterView.af_setImage(withURL: posterUrl!)
     }
     
 
