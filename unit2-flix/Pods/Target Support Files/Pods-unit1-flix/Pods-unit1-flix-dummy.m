130c63eb2d3aeb27647d69b3623be810584942af
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-10-25 15:03:57 -0500

    Prepare for unit2

---
+++ unit2-flix/Pods/Target Support Files/Pods-unit1-flix/Pods-unit1-flix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_unit1_flix : NSObject
+@end
+@implementation PodsDummy_Pods_unit1_flix
+@end
---
+++ unit2-flix/unit1-flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  unit1-flix
+//
+//  Created by Ruolin Zheng on 2019/10/18.
+//  Copyright © 2019 Ruolin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
