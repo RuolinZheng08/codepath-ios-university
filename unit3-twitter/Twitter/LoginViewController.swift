0397562837a1055d4b120d154eac052d4544f6fb
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-11-01 21:21:51 -0500

    Add tweet cell

--- unit3-twitter/Twitter/LoginViewController.swift
+++ unit3-twitter/Twitter/LoginViewController.swift
@@ -16,9 +16,16 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "isLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginSegue", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "isLoggedIn")
             self.performSegue(withIdentifier: "loginSegue", sender: self)
         }, failure: {(Error) in
             print("Could not log in!")
