d4ed87a8ff79829f59e257ac721a58db4d6d4a51
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-11-16 00:27:06 -0600

    Finish feed

--- unit5-pastagram/pastagram/FeedViewController.swift
+++ unit5-pastagram/pastagram/FeedViewController.swift
@@ -6,13 +6,64 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
+import Alamofire
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var posts = [PFObject]()
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 500
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell") as! FeedTableViewCell
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.commentLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.feedImageView.af_setImage(withURL: url)
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        DataRequest.addAcceptableImageContentTypes(["application/octet-stream"])
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if let posts = posts {
+                self.posts = posts
+                self.tableView.reloadData()
+            } else {
+                print("Error loading posts")
+            }
+        }
     }
     
 
